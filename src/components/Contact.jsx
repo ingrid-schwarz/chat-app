89246de5baade8ba219fecad40ae60ce32333ea5
ingrid-schwarz <ingrid.schwarz.wcs@gmail.com>
2020-10-13 16:44:20 +0200

    exo fini

---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Contact.css';
+function Contact() {
+    return <div class="Contact"><img  class="avatar" src="https://randomuser.me/api/portraits/men/12.jpg" alt ="connard de lecteur d'écran"/>
+    <h3 class="name">Lucas Fernandez</h3>
+        <div class="status">
+            <div class="status-online"></div>
+            <p class="status-text">Online</p>
+        </div>
+    </div>
+}
+export default Contact;
