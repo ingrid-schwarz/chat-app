5cc47a15d1f08214a49dada35bd058841260b69e
Schwarzinvader <ingrid.schwarz@caramail.fr>
2020-10-13 21:34:12 +0200

    ajout des aides variables jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 import './Contact.css';
+// Variables
+// let Contact = {
+//   name = 
+//   avatar = <img src="" />,
+//   online = true / false;
+//  isOnline = <p">{online ? 'online' : 'offline'}</p>,
+//  isOText = 
+//  }
+// declaration Contact.name ="X"
+
 function Contact() {
     return <div class="Contact"><img  class="avatar" src="https://randomuser.me/api/portraits/men/12.jpg" alt ="connard de lecteur d'écran"/>
     <h3 class="name">Lucas Fernandez</h3>
