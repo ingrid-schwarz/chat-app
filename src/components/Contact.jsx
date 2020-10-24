f4ebfa9a68f5d9c5970ac89a4a97968d2e92d3bc
Schwarzinvader <ingrid.schwarz@caramail.fr>
2020-10-24 19:27:03 +0200

    contact.jsx devient une fonction

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
 import './Contact.css';
-import PropTypes from 'prop-types'
-function Contact(contact) {  
-    return(
-    <div className="Contact">
-        <p className="name">{contact.name}</p>
-        <img className= "avatar" src={contact.image} alt={contact.name}/>
-        <div className="status">
-            <div className={
+import PropTypes from 'prop-types';
+
+
+class Contact extends React.components{
+    constructor(name, image, status){
+    super(name, image, status);
+    this.state = {
+        online = false;
+        }
+    }
+    
+    render(
+        <div className="Contact">
+            <p className="name">{contact.name}</p>
+            <img className= "avatar" src={contact.image} alt={contact.name}/>
+            <div className="status">
                 contact.status === true ? "status-online" : "status-offline"}/>
             <p className="status-text">{
             contact.status === true ? "online" : "offline"}</p>
+            </div>
         </div>  
-
-    </div>);
-}
-Contact.propTypes = {
+    );
+    
+   Contact.propTypes = {
     name: PropTypes.string.isRequired,
     status: PropTypes.bool.isRequired,
     avatar: PropTypes.string.isRequired
-  };
+    }; 
+};
+
 
 export default Contact;
